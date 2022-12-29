7829461cacb63eacb8b52200599a96c7e77420ee
CodenificienT <61930093+codenificient@users.noreply.github.com>
2022-12-28 21:40:01 -0500

    finish tutorial

---
+++ MachineLearning/NeuralNine/6_neural_networks.py
@@ -0,0 +1,6 @@
+# weights and Balances used on a Neuron, each neuron has an activation funciton which evaluates the result from that neuron
+# using the input from the computation or the result of a previous neuron
+
+
+
+# Gradient descent is used an optimization function for finding the local minimum such that in the end, we have a overral minimum
